7cb2fa76ae1ba439e0a872c3b7947b79f22ac49a
ramonvasc <ramonvasc@gmail.com>
2014-02-26 23:09:51 -0800

    Task 2
    task 2 working

---
+++ lab4 task 2.vhd
@@ -0,0 +1,109 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity lab4 is
+  port(CLOCK_50            : in  std_logic;
+       KEY                 : in  std_logic_vector(3 downto 0);
+       SW                  : in  std_logic_vector(17 downto 0);
+       VGA_R, VGA_G, VGA_B : out std_logic_vector(9 downto 0);  -- The outs go to VGA controller
+       VGA_HS              : out std_logic;
+       VGA_VS              : out std_logic;
+       VGA_BLANK           : out std_logic;
+       VGA_SYNC            : out std_logic;
+       VGA_CLK             : out std_logic);
+end lab4;
+
+architecture rtl of lab4 is
+
+ --Component from the Verilog file: vga_adapter.v
+
+  component vga_adapter
+    generic(RESOLUTION : string);
+    port (resetn                                       : in  std_logic;
+          clock                                        : in  std_logic;
+          colour                                       : in  std_logic_vector(2 downto 0);
+          x                                            : in  std_logic_vector(7 downto 0);
+          y                                            : in  std_logic_vector(6 downto 0);
+          plot                                         : in  std_logic;
+          VGA_R, VGA_G, VGA_B                          : out std_logic_vector(9 downto 0);
+          VGA_HS, VGA_VS, VGA_BLANK, VGA_SYNC, VGA_CLK : out std_logic);
+  end component;
+
+  signal x      : std_logic_vector(7 downto 0);
+  signal y      : std_logic_vector(6 downto 0);
+  signal colour : std_logic_vector(2 downto 0);
+  signal plot   : std_logic;
+  signal int_value : std_logic; 
+begin
+
+  -- includes the vga adapter, which should be in your project 
+
+  vga_u0 : vga_adapter
+    generic map(RESOLUTION => "160x120") 
+    port map(resetn    => KEY(3),
+             clock     => CLOCK_50,
+             colour    => colour,
+             x         => x,
+             y         => y,
+             plot      => plot,
+             VGA_R     => VGA_R,
+             VGA_G     => VGA_G,
+             VGA_B     => VGA_B,
+             VGA_HS    => VGA_HS,
+             VGA_VS    => VGA_VS,
+             VGA_BLANK => VGA_BLANK,
+             VGA_SYNC  => VGA_SYNC,
+             VGA_CLK   => VGA_CLK);
+
+
+  -- rest of your code goes here, as well as possibly additional files
+  process(CLOCK_50,KEY)
+  variable countX : integer range 0 to 159;
+  variable countY : integer range 0 to 119;
+  variable colourVar : std_logic_vector(2 downto 0); 
+  variable int_value : std_logic;
+  begin
+	if(KEY(3) = '0') then
+		int_value := '0';
+   elsif(CLOCK_50'event and CLOCK_50 = '1') then
+	if(KEY(0) = '0') then
+		int_value := '1';
+	end if;
+	case int_value is
+	 when '0' => 
+		if(countX = 159) then
+			countY := countY + 1;
+			countX := 0;
+		else
+			countX := countX + 1;
+		end if;
+		if(countY = 119) then
+			countY := 0;
+		end if;
+		--plot <= '1';
+		colourVar := "000";
+	 when '1' => 
+		if(countX = 159) then
+			countY := countY + 1;
+			countX := 0;
+		else
+			countX := countX + 1;
+		end if;
+		if(countY = 119) then
+			countY := 0;
+		end if;
+		--plot <= '1';
+		colourVar := std_logic_vector(to_unsigned(countY mod 8,3));
+		when others => plot <= '0';
+	 end case;
+		plot <= '1';
+		colour <= colourVar;
+		x <= std_logic_vector(to_unsigned(countX,8));
+		y <= std_logic_vector(to_unsigned(countY,7));
+	 end if;
+	 end process;
+	
+end RTL;
+
+
